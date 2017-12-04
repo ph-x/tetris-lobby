0fadd1b4097f359332fa9272c28bcadb2fd2b45f
Ruoyi Wang <endvroy@gmail.com>
2017-12-03 22:25:20 -0800

    coupled lobby implementation

--- webroot/app/main/lobby.py
+++ webroot/app/main/lobby.py
@@ -1,9 +1,12 @@
 import threading
 from flask_socketio import join_room, leave_room
+from webroot.app.main import main
+from flask_login import current_user
+from flask import send_from_directory, redirect
 
 match_lock = threading.Lock()  # guards the following 2 data structures
-sid_match = {}
-match_nplayers = {}
+uid_match = {}  # uid -> match_id
+match_players = {}  # match_id -> Match
 
 reserved_rooms = {'lobby_chat': 0}  # todo: automatic enumerate
 
@@ -11,46 +14,83 @@ id_lock = threading.Lock()  # guards the match id
 next_match = len(reserved_rooms)
 
 
-def new_match(sid):
+class Match:
+    def __init__(self):
+        self.player1 = None
+        self.player2 = None
+
+    def add(self, uid):
+        if self.player1 is None:
+            self.player1 = uid
+        elif self.player2 is None:
+            self.player2 = uid
+        else:
+            raise RuntimeError('match is full')
+
+    def remove(self, uid):
+        if self.player1 == uid:
+            self.player1 = None
+        elif self.player2 == uid:
+            self.player2 = None
+        else:
+            raise RuntimeError('{} is not a player in this match'.format(uid))
+
+    def is_empty(self):
+        return self.player1 is None and self.player2 is None
+
+    def is_full(self):
+        return self.player1 is not None and self.player2 is not None
+
+
+@main.route('/new_match', methods=['GET'])
+def new_match():
     global next_match
     with id_lock:
         match_id = next_match
         next_match += 1
-    with match_lock:  # may not be necessary
-        sid_match[sid] = match_id
-        match_nplayers[match_id] = 1
-    return match_id
+        match_players[match_id] = Match()  # todo: potentially won't be cleaned
+    return redirect('/match/{}'.format(match_id))
 
 
-def join_match(match_id, sid):
+@main.route('/match/<match_id>')
+def join_match(match_id):
+    uid = current_user.id
     with match_lock:
         try:
-            nplayers = match_nplayers[match_id]
+            players = match_players[match_id]
         except KeyError:
             raise RuntimeError(
-                'user {} wants to join nonexistent match {}'.format(sid, match_id))
+                'user {} wants to join nonexistent match {}'.format(uid, match_id))
         else:
-            if nplayers >= 2:
+            if len(players) >= 2:
                 raise RuntimeError('match {} is full'.format(match_id))
-            if sid in sid_match:
-                raise RuntimeError('user {} already in match {}'.format(sid, sid_match[sid]))
-            join_room(match_id, sid)
-            sid_match[sid] = match_id
+
+            join_room(match_id)
+            uid_match[uid] = match_id
+            match_players[match_id].add(uid)
+    return send_from_directory('static', 'room.html')
 
 
-def leave_match(sid):
+@main.route('/lobby')
+def goto_lobby():
+    uid = current_user.id
     with match_lock:
-        match_id = sid_match[sid]
-        del sid_match[sid]
-        leave_room(match_id, sid)
-        match_nplayers[match_id].nplayers -= 1
-        if match_nplayers[match_id].nplayers == 0:
-            del match_nplayers[match_id]
+        try:
+            match_id = uid_match[uid]  # leave match if in one
+        except KeyError:
+            pass
+        else:
+            del uid_match[uid]
+            leave_room(match_id)
+            match_players[match_id].remove(uid)
+            if match_players[match_id].is_empty():
+                del match_players[match_id]
+    return send_from_directory('static', 'lobby.html')
 
 
 def all_matches():
-    return match_nplayers.keys()
+    return match_players.keys()
 
 
 def all_waiting_matches():
-    return [k for k, v in match_nplayers.items() if v < 2]
+    return [k for k, v in match_players.items() if not v.is_full()]
