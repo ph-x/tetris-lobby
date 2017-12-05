d0c4dee494989de84ffe9b92245e7e38387aa992
xu <zhikuixu@gmail.com>
2017-12-04 17:57:20 -0800

    change database to postgresql, need to be tested

--- webroot/config.py
+++ webroot/config.py
@@ -17,15 +17,12 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,
-                                                                                                'data-dev.sqlite')
+    SQLALCHEMY_DATABASE_URI = 'postgresql:///mydb'
 
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,
-                                                                                                 'data-test.sqlite')
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql:///mydb'
 
 config = {
     'development': DevelopmentConfig,
