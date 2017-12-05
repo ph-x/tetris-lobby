c7734c6b1430420d23362a43c27f7172712553fe
Jiaqi Yang <jya118@sfu.ca>
2017-12-04 16:12:44 -0800

    lobby client done except joining room

--- webroot/app/static/lobby.js
+++ webroot/app/static/lobby.js
@@ -13,14 +13,29 @@ socket.on("user_info", function (data){
     data = JSON.parse(data);
     var user = data['user'];
     //show user name on the block
+    var user_info = document.createElement("li");
+    user_info.innterHTML = user;
+    document.getElementById("user-info").appendChild(user_info);
 });
 
 // update player list
 socket.on("player_list", function (data){
     data = JSON.parse(data);
     var players = data['player_list'];
+
     // remove current player list
+    var player_list = document.getElementById("players");
+    while(player_list.hasChildNodes())
+    {  
+        player_list.removeChild(player_list.firstChild);  
+    } 
+
     // append new player list
+    for(var i = 0; i < player_list.length; i++){
+        var player_info = document.createElement("li");
+    	player_info.innterHTML = players[i];
+    	player_list.appendChild(player_info);
+    }
 });
 
 // update room list
@@ -29,6 +44,31 @@ socket.on("room_list", function (data){
     var rooms = data['room_list'];
     // remove current room list
     // append new room list
+});
+
+// chat msg received
+socket.on("chat_msg", fucntion (data){
+	data = JSON.parse(data);
+    var players = data['player'];
+    var msg = data['message'];
+
+    // make a LI element
+    var msg_node = document.createElement("li");
+
+    var player_info = document.createElement("p");
+    player_info.class = "player";
+    player_info.innterHTML = msg + ":";
+
+    var msg_info = document.createElement("p");
+    msg_info.class = "content";
+    msg_info.innterHTML = msg;
+
+    msg_node.appendChild(player_info);
+    msg_node.appendChild(msg_info);
+
+    // append element to the chat area
+    document.getElementById("chat-msgs").appendChild(msg_node);
+
 });
 
 //////////////////
@@ -42,7 +82,7 @@ document.getElementById("newroom_btn").onclick = function(){
     socket.emit("create_room", {});
 };
 
-// join a room
+// TODO: join a room
 document.getElementById("newroom_btn").onclick = function(){
     // debug
     socket.emit("create_room", {});
@@ -67,4 +107,5 @@ document.getElementById("chat-submit").onclick = function(){
     console.log(chat_msg);
     socket.emit("chat_msg", chat_msg);
 };
+
 
