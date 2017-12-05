5adbbdf50c1f3b448e6f7a6f3c793415115873fe
Ruoyi Wang <endvroy@gmail.com>
2017-12-04 19:18:29 -0800

    password too long fixed

--- webroot/app/models.py
+++ webroot/app/models.py
@@ -7,7 +7,7 @@ class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True, nullable=False)
-    password_hash = db.Column(db.String(64), nullable=False)
+    password_hash = db.Column(db.String(100), nullable=False)
 
     @property
     def password(self):
